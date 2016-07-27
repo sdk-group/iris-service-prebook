095a2dab1c3952a93e56372e05340090a420581b
YuuKohaku <soul.of.bytes@gmail.com>
2016-07-27 13:50:10 +0700

    Slots count fix

--- src/Prebook/stat-method/prebook-slots-count.js
+++ src/Prebook/stat-method/prebook-slots-count.js
@@ -2,7 +2,9 @@ module.exports = {
 	name: 'prebook_slots_count',
 	compute: function (datapart) {
 		let part = datapart.prebook_percentage;
-		return _.clamp(_.floor(part * _.sumBy(datapart.prebook_chunk_mapping, t => (_.floor(t / datapart.prebook_slot_size) - 1))), 0, Infinity);
+		let max_slots = _.floor(_.min([datapart.prebook_available_time, (datapart.live_total_time * part - datapart.reserved)]) / datapart.prebook_slot_size);
+		// console.log("MAXSL", max_slots, datapart.prebook_available_time, (datapart.live_total_time * part - datapart.reserved));
+		return _.clamp(_.sumBy(datapart.prebook_chunk_mapping, t => (_.floor(t / datapart.prebook_slot_size) - 1)), 0, max_slots);
 	}
 
 };
